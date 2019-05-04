22efd24914afcb46a3ae332c0aeed56e39323a3c
ha.vu <ha.vu@quant-edge.com>
2019-05-04 15:40:45 +0700

    add ml book

---
+++ Tài liệu machine learning cơ bản cho người mới bắt đầu_files/69808.js
@@ -0,0 +1,1 @@
+var eafl_public=eafl_public||{};jQuery(document).ready(function($){jQuery('a.eafl-link-direct').on('click',function(){var data={action:'eafl_register_click',security:eafl_public.nonce,link:jQuery(this).data('eafl-id')};jQuery.post(eafl_public.ajax_url,data);});});
